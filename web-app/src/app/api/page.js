a439420b81795d00b502e602a3a54d4920f6f958
crystalhtc <crystalhtc01@gmail.com>
2024-10-10 12:06:09 -0700

    nasa api code

--- web-app/src/app/api/page.js
+++ web-app/src/app/api/page.js
@@ -2,49 +2,45 @@
 import { useState } from 'react';
 
 export default function Home() {
-    //x button to click to call api
-    // function that calls the api
-    //x somewhere to output the data i get back
-    // iterator to work through the data and format it
-    //x display an empty and fulfilled state
-    //x create a state to contain my data
 
-    const [products, setProducts] = useState(null); // null is safer than an empty string
-    const API_URL = 'https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json'
+    const [media, setMedia] = useState(null); // null is safer than an empty string
+    const API_URL = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=5'
 
-    async function fetchProducts() {
+    async function fetchMedia() {
         const response = await fetch(API_URL); 
         // console.log(response); // response is a promise, would be a object(?)
         const data  = await response.json(); // make response into a json object
         // console.log(data);
-        setProducts(data); 
+        setMedia(data); 
     }
     // await is a keyword that can only be used inside an async function
     // await: tells the function to wait for the promise to resolve before continuing, say dont panic until data is gotten
 
-    const ProductOutput = () => {
-        if (products) {
+    const MediaOutput = () => {
+        if (media) {
 
-            let productsList = [];
+            let mediaList = [];
 
-            products.forEach((product, index) => {
-                productsList.push(
+            media.forEach((mediaItem, index) => {
+                mediaList.push(
                     <li key={index}>
-                        {product.name}
+                        <h2 className="text-xl">{mediaItem.title}</h2>
+                        <img src={mediaItem.url} alt={mediaItem.explanation}/>
+                        <p>{mediaItem.explanation}</p>
                     </li>
                 )
             })
 
             return (
                 <div className="p-4 mb-4 border-4 border-black text-center">
-                    <ul>{productsList}</ul>
+                    <ul>{mediaList}</ul>
                 </div>
             )
         }
 
         return (
             <div className="mb-4 border-4 border-black text-center">
-                🌭🌭🌭🌭 NO 🌭🌭🌭 PRODUCTS 🌭🌭🌭🌭 YET 🌭🌭🌭🌭
+                 NO MEDIA YET
             </div>
         ) 
     }
@@ -53,15 +49,15 @@ export default function Home() {
         <div className="p-4 bg-yellow-300">
             Welcome to my API page!
             <header className="p-4 mb-4 border-4 border-black">
-                <h1 className="text-4xl mb-4">Welcome to my product page</h1>
+                <h1 className="text-4xl mb-4">Welcome to my media page</h1>
                 <button
                     className="p-4 bg-black text-yellow-300"
-                    onClick={fetchProducts}
+                    onClick={fetchMedia}
                 >
-                    🌭Fetch products!
+                    Fetch media!
                 </button>
             </header>
-            <ProductOutput />
+            <MediaOutput />
         </div>
     );
 }
